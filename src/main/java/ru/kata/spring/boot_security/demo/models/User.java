756a45485f496eef67143bfe0395aa47a2e8f6b9
NursTaal <nurs.tnt.123@gmail.com>
2023-01-13 19:25:00 +0600

    Убрал @Id Определяет первичный ключ объекта сущности

--- src/main/java/ru/kata/spring/boot_security/demo/models/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/User.java
@@ -14,7 +14,6 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     @Column(name = "id")
-    @Basic(optional = false)
     private Long id;
 
     @Column(name = "user_name")
